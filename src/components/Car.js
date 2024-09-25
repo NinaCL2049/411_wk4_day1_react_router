067fe70613ec1c036ce1b1520eb0027fd2d478bd
NinaCL2049 <ninaclangston@gmail.com>
2024-09-25 17:47:00 -0500

    part one, with the routing code & stuff that I wrote

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
-
-// import MUI components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+import { useParams } from "react-router-dom";
+import { Container, Paper, Chip } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const { id } = useParams();
 
-export default Car
+  const car = cars.find((car) => car.id === Number(id));
+
+  if (!car) {
+    return <div>Car not found</div>;
+  }
+  return (
+    <div class="MuiPaper-root">
+      <Container maxWidth="sm">
+        <Paper>
+          <h2>{car.Name.toUpperCase()}</h2>
+          <Stack direction="row" spacing={1} sx={{ flexWrap: 'wrap' }}>
+            <Chip label={`id: ${car["id"]}`} />
+            <Chip label={`Name: ${car["Name"]}`} />
+            <Chip label={`Miles_per_Gallon: ${car["Miles_per_Gallon"]}`} />
+            <Chip label={`Cylinders: ${car["Cylinders"]}`} />
+            <Chip label={`Displacement: ${car["Displacement"]}`} />
+            <Chip label={`Horsepower: ${car["Horsepower"]}`} />
+            <Chip label={`Weight_in_lbs: ${car["Weight_in_lbs"]}`} />
+            <Chip label={`Acceleration: ${car["Acceleration"]}`} />
+            <Chip label={`Year: ${car["Year"]}`} />
+            <Chip label={`Origin: ${car["Origin"]}`} />
+          </Stack>
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+export default Car;
